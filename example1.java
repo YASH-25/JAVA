1d063aaefc3c2752884482de3bd9e0c404f4a086
YASH UMESHCHANDRA PATHAK <55308802+YASH-25@users.noreply.github.com>
2022-03-12 09:43:58 +0530

    Add files via upload

---
+++ example1.java
@@ -0,0 +1,12 @@
+public class example1{
+public static void main(String arg[]){
+    boolean weekdays = true;
+    boolean weekend = false; 
+    if (!weekdays || weekend) 
+    {
+      System.out.print("Fuck..Bitch");
+    }
+    else
+        System.out.print("Oh yeah....");
+  }
+}
---
+++ floyds.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ 
+class Floyd
+{
+   public static void main(String args[])
+   {
+      int n, num = 1, i, j;
+      Scanner in = new Scanner(System.in);
+      System.out.println("Enter the number of rows for floyd's triangle");
+      n = in.nextInt();
+      System.out.println("Floyd's triangle :-");
+      for ( i = 1 ; i <= n ; i++ )
+      {
+         for ( j = 1 ; j <= i ; j++ )
+           {
+              System.out.print(num+" ");
+              num++;
+           }
+          System.out.println();
+      }
+   }
+}
